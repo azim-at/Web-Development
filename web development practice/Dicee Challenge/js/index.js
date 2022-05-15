f5c66a09dcfaf6c8f8e6b0444dc31ca12a781613
azim-at <azeem.talikoti@gmail.com>
2022-05-15 13:55:09 +0530

    script for dice roll

--- web development practice/Dicee Challenge/js/index.js
+++ web development practice/Dicee Challenge/js/index.js
@@ -2,27 +2,27 @@ function globalFunction(){
     var randomNumber1 = Math.floor(Math.random()* 6 + 1);
     var randomNumber2 = Math.floor(Math.random()* 6 + 1);
         if (randomNumber1 === 1) {
-                    document.querySelector("#player1").setAttribute("src","images/dice1.png");
+            document.querySelector("#player1").setAttribute("src","images/dice1.png");
         }else if (randomNumber1 === 2) {
-                    document.querySelector("#player1").setAttribute("src","images/dice2.png");
+            document.querySelector("#player1").setAttribute("src","images/dice2.png");
         }else if (randomNumber1 === 3) {
-                    document.querySelector("#player1").setAttribute("src","images/dice3.png");
+            document.querySelector("#player1").setAttribute("src","images/dice3.png");
         }else if (randomNumber1 === 4) {
-                    document.querySelector("#player1").setAttribute("src","images/dice4.png");
+            document.querySelector("#player1").setAttribute("src","images/dice4.png");
         }else if (randomNumber1 === 5) {
-                    document.querySelector("#player1").setAttribute("src","images/dice5.png");
+            document.querySelector("#player1").setAttribute("src","images/dice5.png");
         }else {
-                    document.querySelector("#player1").setAttribute("src","images/dice6.png");
+            document.querySelector("#player1").setAttribute("src","images/dice6.png");
         }
 
         if (randomNumber2 === 1) {
-                    document.querySelector("#player2").setAttribute("src","images/dice1.png");
+            document.querySelector("#player2").setAttribute("src","images/dice1.png");
         }else if (randomNumber2 === 2) {
-                    document.querySelector("#player2").setAttribute("src","images/dice2.png");
+            document.querySelector("#player2").setAttribute("src","images/dice2.png");
         }else if (randomNumber2 === 3) {
-                    document.querySelector("#player2").setAttribute("src","images/dice3.png");
+            document.querySelector("#player2").setAttribute("src","images/dice3.png");
         }else if (randomNumber2 === 4) {
-                    document.querySelector("#player2").setAttribute("src","images/dice4.png");
+            document.querySelector("#player2").setAttribute("src","images/dice4.png");
         }else if (randomNumber2 === 5) {
             document.querySelector("#player2").setAttribute("src","images/dice5.png");
         }else {
