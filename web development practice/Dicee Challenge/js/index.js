4c1c1a939f78985e3bdf7a1c1eb063c88feef911
azim-at <azeem.talikoti@gmail.com>
2022-05-15 13:54:33 +0530

    script for dice roll

--- web development practice/Dicee Challenge/js/index.js
+++ web development practice/Dicee Challenge/js/index.js
@@ -19,21 +19,21 @@ function globalFunction(){
                     document.querySelector("#player2").setAttribute("src","images/dice1.png");
         }else if (randomNumber2 === 2) {
                     document.querySelector("#player2").setAttribute("src","images/dice2.png");
-                }else if (randomNumber2 === 3) {
+        }else if (randomNumber2 === 3) {
                     document.querySelector("#player2").setAttribute("src","images/dice3.png");
-                }else if (randomNumber2 === 4) {
+        }else if (randomNumber2 === 4) {
                     document.querySelector("#player2").setAttribute("src","images/dice4.png");
-                }else if (randomNumber2 === 5) {
-                    document.querySelector("#player2").setAttribute("src","images/dice5.png");
-                }else {
-                    document.querySelector("#player2").setAttribute("src","images/dice6.png");
-                }
+        }else if (randomNumber2 === 5) {
+            document.querySelector("#player2").setAttribute("src","images/dice5.png");
+        }else {
+            document.querySelector("#player2").setAttribute("src","images/dice6.png");
+        }
                 
-                if ( randomNumber1 > randomNumber2) {
-                    document.querySelector("h1").innerHTML = "Player 1 wins the game!";
-                } else if ( randomNumber2 > randomNumber1) {
-                    document.querySelector("h1").innerHTML = "Player 2 wins the game!";
-                }else {
-                    document.querySelector("h1").innerHTML = "It's a tie!";
-                }
+        if ( randomNumber1 > randomNumber2) {
+            document.querySelector("h1").innerHTML = "Player 1 wins the game!";
+        } else if ( randomNumber2 > randomNumber1) {
+            document.querySelector("h1").innerHTML = "Player 2 wins the game!";
+        }else {
+            document.querySelector("h1").innerHTML = "It's a tie!";
+        }
 } 
