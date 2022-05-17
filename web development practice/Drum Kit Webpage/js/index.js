0edfcefa5ae614b3e7e44c3006815a8c7331a951
azim-at <azeem.talikoti@gmail.com>
2022-05-17 21:59:02 +0530

    added keyboard events

--- web development practice/Drum Kit Webpage/js/index.js
+++ web development practice/Drum Kit Webpage/js/index.js
@@ -1,47 +1,55 @@
 for(i = 0; i < document.querySelectorAll(".drum").length; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", onClick);
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        var buttonHTML = this.innerHTML;
+        keyPress(buttonHTML);
+    });
 }
 
-function onClick() {
-var buttonHTML = this.innerHTML;
-// Switch Statement
-switch (buttonHTML) {
+document.addEventListener("keydown",function(event) {
+    keyPress(event.key);
+})
+
+
+
+
+function keyPress (key) {
+switch (key) {
         case 'w':
-            var audio = new Audio('sounds/crash.mp3');
-            audio.play();
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
             break;
 
         case 'a':
-            var audio = new Audio('sounds/kick-bass.mp3');
-            audio.play();
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
             break;
 
         case 's':
-            var audio = new Audio('sounds/snare.mp3');
-            audio.play();
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
             break;
 
         case 'd':
-            var audio = new Audio('sounds/tom-1.mp3');
-            audio.play();
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
             break;
 
         case 'j':
-        var audio = new Audio('sounds/tom-2.mp3');
-        audio.play();
+        var tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play();
         break;
 
         case 'k':
-            var audio = new Audio('sounds/tom-3.mp3');
-            audio.play();
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
             break;
 
         case 'l':
-            var audio = new Audio('sounds/tom-4.mp3');
-            audio.play();
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
             break;
     default:
-        alert("You are pressing the wrong button");
+        console.log("You are pressing the wrong button");
         break;
 }
 }
